6d7b8894b0cef44f56ab6cb55125e67389aa9b06
小严 <YWHMatrix@Gmail.com>
2023-08-13 21:48:02 +0800

    0.3 -- 重写Paint方法，画出一个有眼神的圆

--- src/TankClient/TankClients.java
+++ src/TankClient/TankClients.java
@@ -5,9 +5,16 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankClients extends Frame {
+    public static void main(String[] args) {
+        TankClients  t = new TankClients();
+        t.ShowWindow();
+        t.OffWindow();
+    }
     public void ShowWindow(){
         this.setLocation(400,300);//相对于原地的位置
         this.setSize(800,600);
+        this.setTitle("TankBattle");
+        this.setBackground(Color.green);
         setVisible(true); //显示窗口
         setResizable(false);//是否可以由用户调节窗口
     }
@@ -21,13 +28,10 @@ public class TankClients extends Frame {
         });
     }
 
-    public static void main(String[] args) {
-        TankClients  t = new TankClients();
-        t.ShowWindow();
-        t.OffWindow();
+    @Override
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(30,30,40,40);
     }
-
-
-
-
 }
