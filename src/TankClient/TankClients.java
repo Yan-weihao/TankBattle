d3ec00219c56dd607b7fb0caaabf336c7a979dbe
小严 <YWHMatrix@Gmail.com>
2023-08-13 21:20:28 +0800

    0.2 -- 添加窗口关闭功能和禁止用户调节窗口大小

--- src/TankClient/TankClients.java
+++ src/TankClient/TankClients.java
@@ -1,16 +1,33 @@
 package TankClient;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class TankClients extends Frame {
     public void ShowWindow(){
         this.setLocation(400,300);//相对于原地的位置
         this.setSize(800,600);
         setVisible(true); //显示窗口
+        setResizable(false);//是否可以由用户调节窗口
+    }
+
+    public void OffWindow(){
+        this.addWindowListener(new WindowAdapter() {//使用一个匿名类相应关闭窗口事件
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
 
     public static void main(String[] args) {
         TankClients  t = new TankClients();
         t.ShowWindow();
+        t.OffWindow();
     }
+
+
+
+
 }
